4801763f6f4ebe9168f5f4fe36b12c9413b0b43d
djawad <ha191952@ens.uqam.ca>
2025-02-06 08:52:31 -0500

    refactoring

--- src/test/java/ca/uqam/mgl7230/tp1/config/FileWriterProviderTest.java
+++ src/test/java/ca/uqam/mgl7230/tp1/config/FileWriterProviderTest.java
@@ -3,7 +3,6 @@ package ca.uqam.mgl7230.tp1.config;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.FileWriter;
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
